598d0c2badcdeffd20594cb1deae6e46ff30925f
Medve <medvedev.ilya@rambler.ru>
2021-08-12 20:26:14 +0300

    add mode production condition

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -25,7 +25,7 @@ const cssLoaders = loader => {
 
 
 module.exports = {
-    mode: 'development',
+    mode: isDev ? 'development' : 'production',
     entry: {
         main: path.resolve('./src/app.js')
 
